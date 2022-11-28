16ffb77c4d602793bb1e406a7021fc005fd79b2a
Padmavathi9908 <103577909+Padmavathi9908@users.noreply.github.com>
2022-11-28 16:57:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+
+      <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+    </div>
+  );
+}
+
+export default App;
